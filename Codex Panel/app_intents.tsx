92337df303c1bf91a3e8a9d1d409b6c191fc0334
Jason Liu <lxjsilver@gmail.com>
2026-08-18 19:51:11 +0800

    chore: remove legacy Codex Panel folder (renamed to Codex-Panel)

--- Codex Panel/app_intents.tsx
+++
@@ -1,9 +0,0 @@
-import { AppIntentManager, AppIntentProtocol, Script, Widget } from "scripting";
-
-export const ReloadIntent = AppIntentManager.register({
-  name: Script.name,
-  protocol: AppIntentProtocol.AppIntent,
-  perform: async () => {
-    Widget.reloadUserWidgets();
-  },
-});
